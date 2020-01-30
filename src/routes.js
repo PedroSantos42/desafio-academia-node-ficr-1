c16585a86a25a0571b2864e897d5320bf3d0ae07
Pedro <pedroabs.dev42@gmail.com>
2020-01-30 14:19:11 -0300

    Extraindo dados para github profile

--- src/routes.js
+++ src/routes.js
@@ -5,26 +5,44 @@ const routes = Router()
 
 routes.get('/', (req, res, next) => res.send('Hello, desafio Nodejs'))
 
-routes.get('/github/user', async (req, res, next) => {
-    // request a api publica do github utilizando axios
+routes.get('/github', async (req, res, next) => {
+
+    let repos = []
+    let name, bio, company
+
     try {
         const { data } = await axios.get('https://api.github.com/users/PedroSantos42');
-        return res.send(data);
+        company = data.company
+        name = data.name
+        bio = data.bio
     } catch (error) {
         console.log('error', error)
         return res.send(error);
     }
-})
 
-routes.get('/github/repos', async (req, res, next) => {
-    // request a api publica do github utilizando axios
     try {
         const { data } = await axios.get('https://api.github.com/users/PedroSantos42/repos');
-        return res.send(data);
+
+        repos = data.map(repo => {
+            let r = {
+                size: repo.size,
+                name: repo.name,
+                url: repo.url
+            }
+            return r
+        }).sort((a, b) => {
+            if (a.size < b.size)
+                return 1
+            if (a.size > b.size)
+                return -1
+            return 0;
+        }).slice(0, 3)
     } catch (error) {
         console.log('error', error)
         return res.send(error);
     }
+    // console.log('repos', repos)
+    // console.log('user-info', `name: ${name}, bio: ${bio}, company: ${company}`)
 })
 
 module.exports = routes
