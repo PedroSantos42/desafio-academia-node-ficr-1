b8c23a2e68b17381d93b3b5349a3f161879449c1
Pedro <pedroabs.dev42@gmail.com>
2020-01-31 02:18:11 -0300

    Middleware

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const Router = require('express')
 const CurriculoController = require('./controllers/CurriculoController')
+const NotFound = require('../middlewares/NotFound')
 
 const routes = Router()
+
+routes.use(NotFound)
 
 routes.get('/curriculo', CurriculoController.curriculo)
 
